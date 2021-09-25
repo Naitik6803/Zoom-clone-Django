d89a673bcb286e623ee12c2c60ef7831fa03882e
Naitik <naitikpatil6803@gmail.com>
2021-09-25 12:50:19 +0530

    first

--- Mysite/Chat/views.py
+++ Mysite/Chat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main_view(request):
+    context={}
+    return render(request,'Chat/main.html',context=context)
--- Mysite/Mysite/settings.py
+++ Mysite/Mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Chat',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'Static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ASGI_APPLICATION='Mysite.asgi.application'
